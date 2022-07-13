8d121d6087238bb1eb7411ccf768dca0e73998e3
joao0otavio3@gmail.com <joao0otavio3@gmail.com>
2022-07-13 18:41:34 -0300

    adiciona elementos corretos na Fase2 e remove atributo posicaoAbrePorta da classe Fase

--- src/Modelo/Fase.java
+++ src/Modelo/Fase.java
@@ -1,11 +1,8 @@
 package Modelo;
 
-import Auxiliar.Posicao;
-
 import java.util.ArrayList;
 
 public abstract class Fase extends ArrayList<Elemento> {
   public int coracoesRestantes;
   public boolean proximaFase;
-  public Posicao posicaoAbrePorta;
 }
--- src/Modelo/Fase2.java
+++ src/Modelo/Fase2.java
@@ -1,20 +1,35 @@
 package Modelo;
 
+import Auxiliar.Posicao;
+
 public class Fase2 extends Fase {
     public Fase2() {
-        this.coracoesRestantes = 7;
         /*Cria e adiciona personagens*/
+
+        this.coracoesRestantes = 7;
         Lolo lLolo = new Lolo();
-        lLolo.setPosicao(8, 6);
+        lLolo.setPosicao(11, 11);
         this.add(lLolo);
 
         Bau b = new Bau();
-        b.setPosicao(3, 6);
+        b.setPosicao(1, 1);
         this.add(b);
 
         Porta p = new Porta();
-        p.setPosicao(0, 6);
+        p.setPosicao(0, 7);
         this.add(p);
+
+        Cobrinha c1 = new Cobrinha();
+        c1.setPosicao(7, 5);
+        this.add(c1);
+
+        Caveira ca1 = new Caveira();
+        ca1.setPosicao(3, 5);
+        this.add(ca1);
+
+        Caveira ca2 = new Caveira();
+        ca2.setPosicao(6, 8);
+        this.add(ca2);
 
         adicionaMoitasBrancas();
         adicionaMoitasVerdes();
@@ -28,20 +43,24 @@ public class Fase2 extends Fase {
     }
 
     private void adicionaMoitasBrancas() {
-        adicionaMoitaBranca(1, 1);
-        adicionaMoitaBranca(3, 1);
-        adicionaMoitaBranca(10, 1);
         adicionaMoitaBranca(11, 1);
         adicionaMoitaBranca(11, 4);
-        adicionaMoitaBranca(10, 5);
-        adicionaMoitaBranca(11, 5);
-        adicionaMoitaBranca(10, 6);
-        adicionaMoitaBranca(2, 8);
+        adicionaMoitaBranca(11, 9);
+        adicionaMoitaBranca(10, 2);
+        adicionaMoitaBranca(8, 3);
+        adicionaMoitaBranca(8, 5);
+        adicionaMoitaBranca(8, 6);
+        adicionaMoitaBranca(8, 8);
+        adicionaMoitaBranca(6, 4);
+        adicionaMoitaBranca(6, 9);
+        adicionaMoitaBranca(5, 1);
+        adicionaMoitaBranca(5, 10);
+        adicionaMoitaBranca(4, 2);
+        adicionaMoitaBranca(4, 4);
+        adicionaMoitaBranca(4, 6);
+        adicionaMoitaBranca(3, 2);
         adicionaMoitaBranca(3, 8);
-        adicionaMoitaBranca(3, 9);
-        adicionaMoitaBranca(4, 9);
-        adicionaMoitaBranca(10, 10);
-        adicionaMoitaBranca(9, 11);
+        adicionaMoitaBranca(2, 10);
     }
 
     private void adicionaMoitaVerde(int linha, int coluna) {
@@ -51,28 +70,35 @@ public class Fase2 extends Fase {
     }
 
     private void adicionaMoitasVerdes() {
-        adicionaMoitaVerde(4, 1);
-        adicionaMoitaVerde(7, 1);
-        adicionaMoitaVerde(8, 1);
-        adicionaMoitaVerde(9, 1);
-        adicionaMoitaVerde(8, 2);
-        adicionaMoitaVerde(9, 2);
-        adicionaMoitaVerde(2, 3);
-        adicionaMoitaVerde(3, 3);
+        adicionaMoitaVerde(1, 2);
+        adicionaMoitaVerde(1, 4);
+        adicionaMoitaVerde(1, 5);
+        adicionaMoitaVerde(1, 6);
+        adicionaMoitaVerde(1, 8);
+        adicionaMoitaVerde(2, 6);
         adicionaMoitaVerde(2, 4);
-        adicionaMoitaVerde(3, 4);
-        adicionaMoitaVerde(11, 6);
-        adicionaMoitaVerde(9, 7);
-        adicionaMoitaVerde(10, 7);
-        adicionaMoitaVerde(11, 7);
-        adicionaMoitaVerde(1, 9);
-        adicionaMoitaVerde(2, 9);
-        adicionaMoitaVerde(1, 10);
-        adicionaMoitaVerde(2, 10);
+        adicionaMoitaVerde(2, 8);
         adicionaMoitaVerde(3, 10);
+        adicionaMoitaVerde(4, 8);
+        adicionaMoitaVerde(4, 10);
+        adicionaMoitaVerde(5, 2);
+        adicionaMoitaVerde(5, 6);
+        adicionaMoitaVerde(6, 3);
+        adicionaMoitaVerde(6, 5);
+        adicionaMoitaVerde(6, 7);
+        adicionaMoitaVerde(7, 1);
+        adicionaMoitaVerde(7, 11);
+        adicionaMoitaVerde(8, 4);
+        adicionaMoitaVerde(8, 7);
+        adicionaMoitaVerde(8, 9);
+        adicionaMoitaVerde(8, 10);
+        adicionaMoitaVerde(9, 8);
+        adicionaMoitaVerde(10, 4);
+        adicionaMoitaVerde(10, 6);
+        adicionaMoitaVerde(10, 7);
+        adicionaMoitaVerde(10, 10);
+        adicionaMoitaVerde(11, 2);
         adicionaMoitaVerde(11, 10);
-        adicionaMoitaVerde(10, 11);
-        adicionaMoitaVerde(11, 11);
     }
 
     private void adicionaCoracao(int linha, int coluna) {
@@ -82,11 +108,12 @@ public class Fase2 extends Fase {
     }
 
     private void adicionaCoracoes() {
-        adicionaCoracao(2, 1);
-        adicionaCoracao(11, 2);
-        adicionaCoracao(1, 8);
-        adicionaCoracao(11, 8);
-        adicionaCoracao(11, 9);
-        adicionaCoracao(1, 11);
+        adicionaCoracao(1, 3);
+        adicionaCoracao(1, 7);
+        adicionaCoracao(6, 1);
+        adicionaCoracao(6, 6);
+        adicionaCoracao(8, 11);
+        adicionaCoracao(9, 7);
+        adicionaCoracao(10, 1);
     }
 }
